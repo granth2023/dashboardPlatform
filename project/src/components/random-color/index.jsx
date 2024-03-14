2067b620842928133fc55da66188bb28289a6588
granth2023 <grantharris38@gmail.com>
2024-03-14 10:04:30 -0400

    add notes

--- project/src/components/random-color/index.jsx
+++ project/src/components/random-color/index.jsx
@@ -4,34 +4,43 @@ import { useEffect, useState } from 'react'
 
 
 export default function RandomColor(){
+    //default type of color creation is hex 
     const [typeOfColor, setTypeOfColor] = useState('hex');
+    //black is default color 
     const [color, setColor] = useState('#000000')
-
+//need to be able to get random colors within the range of possible values
     function randomColorUtility(length) {
+        //use math methods and return it times the length
         return Math.floor(Math.random()* length) 
     }
-
+//create random hex
         function handleCreateRandomHexColor(){
+            //what is the range of possible values for hex array
             const hex = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+            //dont forget your # and store it
             let hexColor = "#"
-
+///loop through for 6 values and add to the hex color the randomization within the length of possible values
             for(let i=0; i<6; i++){
                 hexColor += hex[randomColorUtility(hex.length)];
 
             }
             console.log(hexColor)
 
+            //set the color to hexcolor now that its been created
+
             setColor(hexColor)
         }
-
+//create random rgb
         function handleCreateRandomRgbColor(){
+            //store randomization function holding the length of rgb vlaues in 3 distinct variable r g b
             const r = randomColorUtility(256);
             const g = randomColorUtility(256);
             const b = randomColorUtility(256);
-
+        // function stores random value in each of the three variables and then set the color
             setColor(`rgb${r}, ${g}, ${b}`)
 
         }
+        // if we're in rgb creation handle create random rgb if twe're in hex create that and set the type of color for use effect 
 
         useEffect(() => {
 
