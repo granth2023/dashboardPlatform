63e306f058c170a178c07e3e72d457c47d23546d
granth2023 <grantharris38@gmail.com>
2024-03-17 12:07:09 -0400

    states and effect and error

--- project/src/components/imageSlider/index.jsx
+++ project/src/components/imageSlider/index.jsx
@@ -6,15 +6,39 @@ export default function ImageSlider({url, limit}){
 
     const [images, setImages ] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     async function fetchImages(getUrl){
         //error handle
+
+        try{
+            setLoading(true)
+            const response = await fetch(getUrl);
+            const data = await response.json();
+
+            if(data){
+                setImages(data)
+                setLoading(false);
+            }
+        }catch(e){
+                setLoading(false)
+
+
+        }
     }
 
-    useEffect(()= > { 
+    useEffect(()=> { 
         if(url !== '') fetchImages()
 
     },[url])
+
+    if(loading){
+        return<div> Loading Data ! Please wait</div>
+    }
+    if(errorMsg!== null){
+        return<div> Error occured!{errorMsg}</div>
+    }
 
 
     return ( 
