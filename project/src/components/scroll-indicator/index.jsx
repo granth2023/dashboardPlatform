728da5ca4794ad7dc4a94357412d2fc1831e6849
granth2023 <grantharris38@gmail.com>
2024-03-20 18:26:15 -0400

    update

--- project/src/components/scroll-indicator/index.jsx
+++ project/src/components/scroll-indicator/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 
 
 export default function ScrollIndicator({url}){
@@ -5,6 +6,7 @@ export default function ScrollIndicator({url}){
     const [data, setData] = useState([]);
     const [ loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState('')
+    const [ scrollPercentage, setScrollPercentage] = useState(0);
 
     async function fetchData(getUrl){
         try{
@@ -29,9 +31,48 @@ export default function ScrollIndicator({url}){
         fetchData(url);
     },[url])
 
+    function handleScrollPercentage(){
+        //calculate how much and what is full height and get that? 
+        console.log(
+            document.body.scrollTop, 
+            document.documentElement.scrollTop,
+            document.documentElement.scrollHiehgt,
+            document.documentElement.clientHeight)
+
+            const howMuchScrolled = document.body.scrollTop || document.documentElement.scrollTop ;
+
+            const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+            setScrollPercentage((howMuchScrolled/ height) * 100)
+    }
+
+    useEffect(()=> {
+        window.addEventListener("scroll", handleScrollPercentage);
+        return() =>{
+            window.removeEventListener("scroll", ()=> {});
+        }
+    },[])
+
     console.log(data, loading)
+    console.log(scrollPercentage)
     return( 
         <div>
+            <div className="top-container">
+            <h1> Custom Scroll Indicator</h1>
+            <div className="scroll-progress-tracking-container">
+                <div className="current-progress bar" style={{width: `${scrollPercentage}%`}}>
+                    
+                    </div> 
+            </div>
+            </div>
+            <div className="data-container">
+                {
+                    data && data.length > 0 ? 
+                        data.map(dataItem => <p> {dataItem.title} </p>)
+                    :null
+                }
+
+            </div>
 
         </div>
     )
