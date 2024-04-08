37dab53b7f7b8585d3086300c2f2e639bd8b0e51
granth2023 <grantharris38@gmail.com>
2024-04-08 11:46:56 -0400

    firsT

---
+++ dashboardApp/platform/app/desktop/src/view/main/center/CenterView.js
@@ -0,0 +1,6 @@
+Ext.define('Platform.view.main.center.CenterView', {
+	extend: 'Ext.Container',
+	xtype: 'centerview',
+	cls: 'centerview',
+	layout: 'card'
+});
---
+++ dashboardApp/platform/app/shared/src/util/Shared.js
@@ -0,0 +1,8 @@
+Ext.define('Platform.util.Shared', {
+	alternateClassName: ['Shared'],
+	singleton: true,
+
+	log: function(msg) {
+		console.log(msg);
+	}
+});
